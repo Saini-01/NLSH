72a59f8df22ed8d127ac286c1d0d556ccbbd5fa3
Aditya Saini <adityasaini145@gmail.com>
2025-07-12 16:08:18 -0400

    put 1 toe through the door

---
+++ nlsh.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 1024
+
+int main(int argc, char *argv[]){
+
+    char input[MAX_INPUT];
+
+    while(1){
+        printf("nlsh>\n")
+        
+    }
+}
