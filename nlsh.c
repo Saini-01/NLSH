7cfdc9e1f39b33240f5a9dc8037aba3a98e2fc99
Aditya Saini <adityasaini145@gmail.com>
2025-07-12 17:02:43 -0400

    made it actually work a bit

--- nlsh.c
+++ nlsh.c
@@ -5,11 +5,24 @@
 #define MAX_INPUT 1024
 
 int main(int argc, char *argv[]){
-
     char input[MAX_INPUT];
-
+    
     while(1){
-        printf("nlsh>\n");  
+        printf("nlsh> ");
+        fflush(stdout);
+        
+        if(fgets(input, MAX_INPUT, stdin) == NULL) {
+            break; 
+        }
+        
+        input[strcspn(input, "\n")] = 0;
+        
+        if(strcmp(input, "exit") == 0) {
+            break;
+        }
+        
+        printf("You typed: %s\n", input);
     }
     
+    return 0;
 }
