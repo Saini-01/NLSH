5f9dcd4f80aac99ccffc2f29ab0953eca344a2b4
Aditya Saini <adityasaini145@gmail.com>
2025-07-12 23:17:45 -0400

    added initial tokenization

--- nlsh.c
+++ nlsh.c
@@ -4,8 +4,12 @@
 
 #define MAX_INPUT 1024
 
+char* tokenize(char *tokens, char string[MAX_INPUT]);
+
 int main(int argc, char *argv[]){
+    
     char input[MAX_INPUT];
+    char *tokens;
     
     while(1){
         printf("nlsh> ");
@@ -17,12 +21,26 @@ int main(int argc, char *argv[]){
         
         input[strcspn(input, "\n")] = 0;
         
-        if(strcmp(input, "exit") == 0) {
+        if(strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
             break;
         }
         
-        printf("You typed: %s\n", input);
+        tokenize(tokens, input);
+
     }
     
     return 0;
+}
+
+char* tokenize(char *tokens, char string[MAX_INPUT]){
+
+    tokens = strtok(string, " ");
+
+    while(tokens != NULL){
+        printf("%s\n", tokens);
+        tokens = strtok(NULL, " ,-");
+    }
+
+    return tokens;
+
 }
